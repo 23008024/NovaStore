1969055f16798c116eacbb82eec07f1dd88f5838
Naftali <163560278+23008024@users.noreply.github.com>
2026-08-04 14:10:42 +0200

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
-const cors = require("cors");
+app.use(
+    cors({
+        origin: [
+            "https://novastore-1.onrender.com",
+            "http://localhost:5173"
+        ],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
