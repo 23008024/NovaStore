aa34692f16cf2198b30daca3ee9a5c4453496024
Naftali <163560278+23008024@users.noreply.github.com>
2026-08-05 05:24:41 +0200

    Refactor axios instance configuration and logging

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,40 +1,22 @@
 import axios from "axios";
 
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-
     baseURL: import.meta.env.VITE_API_URL,
-
     headers: {
-
         "Content-Type": "application/json"
-
     }
-
 });
 
-
-
-// Attach JWT token automatically
-
-api.interceptors.request.use((config)=>{
-
-
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
-
-    if(token){
-
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
-
     }
 
-
     return config;
-
-
 });
-
-
 
 export default api;
